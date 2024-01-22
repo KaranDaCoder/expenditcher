2d88ec9de7c0a90d116199ff7a01dcfd035c573b
Karan Khot <karankhot@Karans-MacBook-Air.local>
2024-01-21 19:10:10 -0600

    7th

---
+++ src/app/api/expenses/route.js
@@ -0,0 +1,12 @@
+import { connectDb } from "@/lib/dbConnect"
+import { NextResponse } from "next/server";
+
+export const GET = async(request, {params}) => {
+ await connectDb();
+ try {
+  return new NextResponse(JSON.stringify('success') ,{status:200})
+ } catch (error) {
+  
+  return new NextResponse(JSON.stringify('Soemthing went wrong!') ,{status:500})
+ }
+}

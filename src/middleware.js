1d6993e115d9b79c52aafc3c939a2a901d065acc
Karan Khot <karankhot@Karans-MacBook-Air.local>
2024-01-23 01:54:20 -0600

    10th

--- src/middleware.js
+++ src/middleware.js
@@ -1,23 +1,4 @@
-import { NextResponse } from 'next/server';
-
 export { default } from 'next-auth/middleware';
 export const config = {
-  matcher: ['/dashboard/:path*', '/manage'],
+  matcher: ['/dashboard/:path*', '/manage' , '/add-expense'],
 };
-
-// export function middleware(request) {
-//   let value = '';
-//   if (process.env.NODE_ENV === 'production') {
-//     value = request.cookies.get('__Secure-next-auth.session-token').value;
-//   } else {
-//     value = request.cookies.get('next-auth.session-token').value;
-//   }
-//   const response = NextResponse.next();
-//   response.cookies.set({
-//     name: '__Secure-next-auth.session-token',
-//     value,
-//     path: '/',
-//   });
-//   // console.log(response.cookies.get('__Secure-next-auth.session-token'));
-//   return response;
-// }
